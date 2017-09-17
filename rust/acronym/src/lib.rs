d9ecae1da1b3723ee02fbe895e1b72568edca125
endrin@home <yon.drin@gmail.com>
2017-09-17 12:35:09 +0300

    rust/acronym: add initial solution

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,7 +1,30 @@
+use std::iter;
+
 pub fn abbreviate(phrase: &str) -> String {
-    phrase
-        .split(|c: char| c.is_whitespace() || c == '-')
-        .flat_map(|word| word.chars().take(1))
+    // Trim extra spaces from the phrase
+    let phrase = phrase
+        .trim()
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .join(" ");
+    let phrase_bytes = phrase.as_bytes();
+
+    // First character is always included in acronym
+    let first = iter::once(phrase_bytes[0] as char);
+
+    first
+        .chain((0..phrase.len() - 1).filter_map(|idx| {
+            let current = phrase_bytes[idx] as char;
+            let next = phrase_bytes[idx + 1] as char;
+            if current.is_whitespace() || current == '-' ||
+                (current.is_lowercase() &&
+                    next.is_uppercase())
+            {
+                Some(next)
+            } else {
+                None
+            }
+        }))
         .flat_map(char::to_uppercase)
         .collect()
 }
