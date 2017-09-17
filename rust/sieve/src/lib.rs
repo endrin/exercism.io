3dfb954fb817405571359b93f21b5deabec2d9f8
endrin@home <yon.drin@gmail.com>
2017-09-17 13:03:28 +0300

    rust/sieve: add initial solution

---
+++ rust/sieve/src/lib.rs
@@ -0,0 +1,10 @@
+// Already implemented this while solving Nth Prime.
+// Taking sieve algorithm from that solution
+// http://exercism.io/submissions/c1307609d2d0454abe905c43f30aa613
+
+mod primes;
+use primes::Primes;
+
+pub fn primes_up_to(n: usize) -> Vec<usize> {
+    Primes::new().take_while(|p| p <= &n).collect()
+}
---
+++ rust/sieve/src/primes.rs
@@ -0,0 +1,44 @@
+// Tried to implement infinite primes sequence
+// using Iterator and the Sieve of Eratosthenes
+
+use std::collections::HashMap;
+
+pub struct Primes {
+    current: usize,
+    not_primes: HashMap<usize, usize>,
+}
+
+impl Primes {
+    pub fn new() -> Primes {
+        Primes {
+            current: 2,
+            not_primes: HashMap::new(),
+        }
+    }
+}
+
+impl Iterator for Primes {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for q in self.current.. {
+            if let Some(p) = self.not_primes.remove(&q) {
+                let mut x = q.checked_add(p);
+                while x.is_some() &&
+                    self.not_primes
+                        .contains_key(&x.unwrap())
+                {
+                    x = x.and_then(|x_| x_.checked_add(p));
+                }
+                x.map(|x_| self.not_primes.insert(x_, p));
+            } else {
+                q.checked_mul(q).map(|q_squared| {
+                    self.not_primes.insert(q_squared, q)
+                });
+                self.current = q + 1;
+                return Some(q);
+            }
+        }
+        unreachable!();
+    }
+}
