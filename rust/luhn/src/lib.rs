6f9050c1f7d28dbad702ecbd9d89ad852a0c5dce
endrin@home <yon.drin@gmail.com>
2017-09-21 02:04:22 +0300

    Well, why shall I loop through true-false cycle and make decisions based on it if I can loop through transformation arrays directly and just index everything? Implement this idea.

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,21 +1,18 @@
-static DOUBLED: [usize; 10] =
-    [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+const NORMAL: [u32; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
+static TRANSFORMATIONS: [[u32; 10]; 2] = [NORMAL, DOUBLED];
 
 pub fn is_valid(input: &str) -> bool {
     cleaned(input)
         .map(|digits| {
-            let double_these =
-                [false, true].iter().cloned().cycle();
+            let transformations =
+                TRANSFORMATIONS.iter().cycle();
 
             digits
                 .into_iter()
-                .zip(double_these)
-                .map(|(c, double_it)| if double_it {
-                    DOUBLED[c]
-                } else {
-                    c
-                })
-                .sum::<usize>() % 10 == 0
+                .zip(transformations)
+                .map(|(c, transform)| transform[c])
+                .sum::<u32>() % 10 == 0
         })
         .unwrap_or(false)
 }
