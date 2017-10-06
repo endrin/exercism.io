c5f81db4e43b9d6613616985a54bd5742f27e96d
endrin@home <yon.drin@gmail.com>
2017-10-06 10:28:23 +0300

    rust/allergies: change is_allergic_to into checking of bits and get rid of external crates and hashmap

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,9 +1,5 @@
-#[macro_use]
-extern crate lazy_static;
-
-use std::collections::BTreeMap;
-
-#[derive(Clone, Debug, Eq, Ord, PartialOrd, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(u8)]
 pub enum Allergen {
     Eggs,
     Peanuts,
@@ -17,53 +13,38 @@ pub enum Allergen {
 
 use Allergen::*;
 
-lazy_static! {
-    static ref ALLERGY_FLAGS: BTreeMap<Allergen, usize> = {
-        [(Eggs, 0),
-        (Peanuts, 1),
-        (Shellfish, 2),
-        (Strawberries, 3),
-        (Tomatoes, 4),
-        (Chocolate, 5),
-        (Pollen, 6),
-        (Cats, 7)].iter().cloned().collect()
-    };
-}
-
 pub struct Allergies {
-    flags: Vec<u8>,
+    code: u8,
 }
 
 impl Allergies {
     pub fn new(code: u32) -> Self {
         Allergies {
-            flags: {
-                let mut flags = Vec::with_capacity(8);
-                let mut code = code % 256;
-                while code > 0 {
-                    flags.push((code % 2) as u8);
-                    code /= 2;
-                }
-                flags.resize(8, 0);
-                flags
-            },
+            code: (code % 256) as u8,
         }
     }
 
     pub fn is_allergic_to(&self, a: &Allergen) -> bool {
-        self.flags[ALLERGY_FLAGS[a]] == 1
+        let bit = 1 << *a as u8;
+        self.code & bit == bit
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        ALLERGY_FLAGS
-            .iter()
-            .filter_map(
-                |(a, _)| if self.is_allergic_to(&a) {
-                    Some(a.clone())
-                } else {
-                    None
-                },
-            )
+        [
+            Eggs,
+            Peanuts,
+            Shellfish,
+            Strawberries,
+            Tomatoes,
+            Chocolate,
+            Pollen,
+            Cats,
+        ].into_iter()
+            .filter_map(|a| if self.is_allergic_to(a) {
+                Some(*a)
+            } else {
+                None
+            })
             .collect()
     }
 }
