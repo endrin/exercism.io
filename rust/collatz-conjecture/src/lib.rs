63b383e6091601702ffb7aca9d42c9d95155e236
endrin@work <yon.drin@gmail.com>
2017-10-18 18:59:52 +0300

    rust/collatz-conjecture: add initial solution

---
+++ rust/collatz-conjecture/src/lib.rs
@@ -0,0 +1,32 @@
+pub fn collatz(n: u64) -> Result<u64, &'static str> {
+    if n == 0 {
+        Err("Argument must be greater than 0")
+    } else {
+        Ok(CollatzSteps::new(n).count() as u64)
+    }
+}
+
+struct CollatzSteps(u64);
+
+impl CollatzSteps {
+    fn new(n: u64) -> Self {
+        CollatzSteps(n)
+    }
+}
+
+impl Iterator for CollatzSteps {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 == 1 {
+            None
+        } else {
+            if self.0 % 2 == 0 {
+                self.0 /= 2;
+            } else {
+                self.0 = self.0 * 3 + 1;
+            }
+            Some(self.0)
+        }
+    }
+}
