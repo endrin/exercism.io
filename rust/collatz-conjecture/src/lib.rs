3de5fba5d4ac3008d86bd6bfd77c0867e702820f
endrin@home <yon.drin@gmail.com>
2017-10-20 05:39:14 +0300

    rust/collatz-conjecture: make minor adjustments to the boolinator usage

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -19,7 +19,7 @@ impl Iterator for CollatzSteps {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        (self.0 != 1).as_option().map(|_| {
+        (self.0 != 1).as_some_from(|| {
             self.0 = (self.0 % 2 == 0)
                 .as_some(self.0 / 2)
                 .unwrap_or(self.0 * 3 + 1);
