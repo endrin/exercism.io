4f379502cce6c849b6ee405c35dfaacf50fc9e12
endrin@work <yon.drin@gmail.com>
2017-10-24 21:02:00 +0300

    rust/luhn-trait: make more clean solution with specialization feature

--- rust/luhn-trait/src/lib.rs
+++ rust/luhn-trait/src/lib.rs
@@ -1,9 +1,9 @@
 // Most of the actual code is actually moved
 // from my other Luhn's
 
-extern crate num;
+#![feature(specialization)]
 
-use num::{Integer, NumCast};
+use std::fmt;
 
 const NORMAL: [u32; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const DOUBLED: [u32; 10] = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
@@ -28,94 +28,54 @@ pub trait Luhn {
     }
 }
 
-impl<'a> Luhn for &'a str {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_string(&self)
-    }
-}
-
-impl Luhn for String {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_string(&self)
-    }
-}
-
-impl Luhn for u8 {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_integer(self)
-    }
-}
-
-impl Luhn for u16 {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_integer(self)
-    }
-}
-
-impl Luhn for u32 {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_integer(self)
-    }
-}
-
-impl Luhn for u64 {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_integer(self)
-    }
-}
-
-impl Luhn for usize {
-    fn clean(&self) -> Option<Vec<usize>> {
-        clean_integer(self)
-    }
-}
-
-fn clean_string<'a, T>(input: &'a T) -> Option<Vec<usize>>
+default impl<T> Luhn for T
 where
-    T: ToString,
+    T: fmt::Display,
 {
-    let input: String = input.to_string();
+    fn clean(&self) -> Option<Vec<usize>> {
+        let input: String = format!("{}", self);
 
-    if input.chars().any(|c| !"0123546789 ".contains(c)) {
-        return None;
-    }
+        if input.chars().any(|c| !"0123546789 ".contains(c))
+        {
+            return None;
+        }
 
-    let result: Vec<usize> = input
-        .chars()
-        .filter_map(|c| c.to_digit(10))
-        .map(|n| n as usize)
-        .rev()
-        .collect();
+        let result: Vec<usize> = input
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .map(|n| n as usize)
+            .rev()
+            .collect();
 
-    if result.len() >= 2 {
-        Some(result)
-    } else {
-        None
+        if result.len() >= 2 {
+            Some(result)
+        } else {
+            None
+        }
     }
 }
 
-fn clean_integer<T>(input: &T) -> Option<Vec<usize>>
-where
-    T: Copy + Integer + NumCast,
-{
-    let mut ds = vec![];
-    let mut input: T = input.clone();
+macro_rules! impl_luhn_for_numbers {
+    ($($T:ty),+) => { $(
+        impl Luhn for $T {
+            fn clean(&self) -> Option<Vec<usize>> {
+                let mut input = (*self) as u64;
 
-    while input > num::zero() {
-        ds.push(
-            input
-                .mod_floor(&T::from(10usize).unwrap())
-                .to_usize()
-                .unwrap(),
-        );
-        input = input.div_floor(&T::from(10usize).unwrap());
-        // Around here I started thinking that it was
-        // better to just convert all types to string
-    }
+                let mut ds: Vec<usize> = vec![];
 
-    if ds.len() >= 2 {
-        Some(ds)
-    } else {
-        None
-    }
+                while input > 0 {
+                    ds.push((input % 10u64) as usize);
+                    input /= 10;
+                }
+
+                if ds.len() >= 2 {
+                    Some(ds)
+                } else {
+                    None
+                }
+            }
+        }
+    )+ }
 }
+
+impl_luhn_for_numbers!(u8, u16, u32, u64, usize);
