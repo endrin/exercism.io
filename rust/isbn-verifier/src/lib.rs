cf4187a3ac646e3602e1ad58c7a057b2172e8dcc
endrin@work <yon.drin@gmail.com>
2017-11-07 20:34:09 +0200

    rust/isbn-verifier: start working on solution

---
+++ rust/isbn-verifier/src/lib.rs
@@ -0,0 +1,9 @@
+// Hey, nom is fun!
+
+#[macro_use]
+extern crate nom;
+
+/// Determines whether the supplied string is a valid ISBN number
+pub fn is_valid_isbn(_isbn: &str) -> bool {
+    unimplemented!();
+}
