f7c8205a3a9a579831e52bbd42d19658deb1ba85
endrin @ work <yon.drin@gmail.com>
2018-03-26 18:14:36 +0300

    rust/allergies: change the solution to use the bitflags crate

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,42 +1,45 @@
-#[macro_use]
-extern crate custom_derive;
-#[macro_use]
-extern crate enum_derive;
+#![allow(non_upper_case_globals)]
 
-custom_derive! {
-    #[derive(Copy, Clone, Debug, PartialEq,
-        IterVariants(AllergenVariants))]
-    pub enum Allergen {
-        Eggs,
-        Peanuts,
-        Shellfish,
-        Strawberries,
-        Tomatoes,
-        Chocolate,
-        Pollen,
-        Cats,
+#[macro_use]
+extern crate bitflags;
+
+bitflags! {
+    pub struct Allergen: u32 {
+        const Eggs          = 0b0000_0001;
+        const Peanuts       = 0b0000_0010;
+        const Shellfish     = 0b0000_0100;
+        const Strawberries  = 0b0000_1000;
+        const Tomatoes      = 0b0001_0000;
+        const Chocolate     = 0b0010_0000;
+        const Pollen        = 0b0100_0000;
+        const Cats          = 0b1000_0000;
     }
 }
 
-pub struct Allergies {
-    code: u8,
-}
+pub struct Allergies(Allergen);
 
 impl Allergies {
     pub fn new(code: u32) -> Self {
-        Allergies {
-            code: (code % 256) as u8,
-        }
+        Allergies(Allergen::from_bits_truncate(code))
     }
 
     pub fn is_allergic_to(&self, a: &Allergen) -> bool {
-        let bit = 1 << *a as u8;
-        self.code & bit == bit
+        self.0.contains(*a)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        Allergen::iter_variants()
-            .filter(|a| self.is_allergic_to(a))
+        (0..8)
+            .into_iter()
+            .map(|bit| {
+                Allergen::from_bits_truncate(1 << bit)
+            })
+            .filter_map(|flag| {
+                if self.is_allergic_to(&flag) {
+                    Some(flag)
+                } else {
+                    None
+                }
+            })
             .collect()
     }
 }
